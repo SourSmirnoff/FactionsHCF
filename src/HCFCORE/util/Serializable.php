8d8250237c84e3eac6b9e5785cb2f1bfc84cf22e
BajanVlogs <kjolly544@gmail.com>
2017-07-12 22:06:23 -0400

    Create Serializable.php

---
+++ src/HCFCORE/util/Serializable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FactionsHCF;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\math\Vector3;
+use pocketmine\level\level;
+use pocketmine\level\Position;
+
+abstract class Serializable {
+	abstract function toConfig (); 
+	public static function constructor__ConfigurationSection ($section) // [ConfigurationSection section]
+	{
+		$me = new self();
+		return $me;
+	}
+}
+?>
